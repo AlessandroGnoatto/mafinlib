3490ac71e53f0f4bb540853a1447dbbdf23eef16
AlessandroGnoatto <alessandro@alessandrognoatto.com>
2017-07-26 12:51:56 +0200

    Update doc

--- mafinlib/src/org/mafinlib/utils/ConcreteObservable.java
+++ mafinlib/src/org/mafinlib/utils/ConcreteObservable.java
@@ -6,6 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * This class provides a thread-safe implementation of the observer pattern.
+ * 
+ * When a synchronized method is called, the calling thread attempts to acquire an
+ * exclusive lock on the object. If any other thread holds that lock, then the
+ * calling thread stalls until the lock is released.
+ * 
+ * The code is thread-safe under the assumption that the methods that change
+ * the state of an object are only called from one thread.
+ * 
  * @author Alessandro Gnoatto
  */
 public class ConcreteObservable implements Observable {
@@ -30,18 +39,36 @@ public class ConcreteObservable implements Observable {
   public List<Observer> getObservers() {
     return Collections.unmodifiableList(this.observers);
   }
-
+  
   @Override
   public synchronized void deleteObserver(final Observer observer) {
     observers.remove(observer);
   }
-
+  
   @Override
   public synchronized void deleteObservers() {
     observers.clear();
   }
 
  
+  
+  /**
+   * Notification to the observers.
+   * 
+   * The thread that calls notifyObservers obtains a lock but it does not notify
+   * the observers when holding the lock. Instead, it copies the set of observers
+   * to a local LinkedList and releases a lock, and only then proceeds to iteration
+   * over the local list and observers notification. A code of the form
+   * 
+   * synchronized(this){
+   * 	for(Observer myOberserver : observers){
+   * 		observer.update();
+   * 	}
+   * }
+   * 
+   * would be problematic if another thread attempted to register/unregister observers
+   * while "this" thread notifies observers. 
+   */
   @Override
   public void notifyObservers() {
     List<Observer> copyOfObservers;
